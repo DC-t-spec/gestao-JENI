17ccf7325c0771c7d5740ec1ee1d515784447017
DC-t-spec <dcnet025@gmail.com>
2026-04-03 23:52:40 +0200

    Update supabase-client.js

--- assets/js/core/supabase-client.js
+++ assets/js/core/supabase-client.js
@@ -1,4 +1,4 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 
-export const supabase = createClient(https://kzjgpotspojqssbgvibj.supabase.co, SUPABASE_ANON_KEY);
+export const supabase = createClient(https://kzjgpotspojqssbgvibj.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6amdwb3RzcG9qcXNzYmd2aWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNDUwODgsImV4cCI6MjA5MDgyMTA4OH0.namPqEkvICM038IUuAIbCObvQ-YpPN7ND3jh7k5HiFs);
