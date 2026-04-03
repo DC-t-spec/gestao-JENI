37fcb35d6f6270ba8c887f201a5a67c69ddf0b7a
DC-t-spec <dcnet025@gmail.com>
2026-04-03 23:45:04 +0200

    Create dom.js

---
+++ assets/js/ui/dom.js
@@ -0,0 +1,12 @@
+export const dom = {
+  authView: document.querySelector('#auth-view'),
+  appView: document.querySelector('#app-view'),
+  pageContent: document.querySelector('#page-content'),
+  authFeedback: document.querySelector('#auth-feedback'),
+  pageTitle: document.querySelector('#page-title'),
+  pageSubtitle: document.querySelector('#page-subtitle'),
+  topbarUser: document.querySelector('#topbar-user'),
+  topbarBatch: document.querySelector('#topbar-batch'),
+  sidebarUserName: document.querySelector('#sidebar-user-name'),
+  sidebarUserRole: document.querySelector('#sidebar-user-role'),
+};
